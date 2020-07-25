f2602f42536e9522ed25b99d93f6890ca0bfd71e
Tiago Angelo <kurtis.angelo@gmail.com>
2020-07-25 14:33:54 -0300

    Initialize matches project
    Signed-off-by: Tiago Angelo <kurtis.angelo@gmail.com>

---
+++ matches/src/test/java/br/dev/kurtis/NativeMatchResourceIT.java
@@ -0,0 +1,9 @@
+package br.dev.kurtis;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMatchResourceIT extends MatchResourceTest {
+
+    // Execute the same tests but in native mode.
+}
