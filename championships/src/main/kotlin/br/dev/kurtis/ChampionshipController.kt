8a40b44073a4c125c16e0b3596aa69ee90bb8c5c
Tiago Angelo <kurtis.angelo@gmail.com>
2020-07-31 15:10:48 -0300

    Handling not found championships
    Signed-off-by: Tiago Angelo <kurtis.angelo@gmail.com>

--- championships/src/main/kotlin/br/dev/kurtis/ChampionshipController.kt
+++ championships/src/main/kotlin/br/dev/kurtis/ChampionshipController.kt
@@ -2,8 +2,13 @@ package br.dev.kurtis
 
 import br.dev.kurtis.model.Resources
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Error
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.hateoas.JsonError
+import io.micronaut.http.hateoas.Link
 import java.io.File
 
 @Controller("/championships")
@@ -21,4 +26,10 @@ class ChampionshipController(mapper: ObjectMapper) {
 
     @Get(uri = "/{id}", produces = ["application/hal+json"])
     fun findOne(id: Long) = resources.embedded?.championships?.first { championship -> championship.hasId(id) }
+
+    @Error(exception = java.util.NoSuchElementException::class)
+    fun notFound(request: HttpRequest<*>): HttpResponse<JsonError> {
+        val error = JsonError("Page Not Found").link(Link.SELF, Link.of(request.uri))
+        return HttpResponse.notFound(error)
+    }
 }
--- championships/src/test/kotlin/br/dev/kurtis/ChampionshipControllerTest.kt
+++ championships/src/test/kotlin/br/dev/kurtis/ChampionshipControllerTest.kt
@@ -2,23 +2,40 @@ package br.dev.kurtis
 
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.annotation.MicronautTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 @MicronautTest
-class ChampionshipControllerTest(private val embeddedServer: EmbeddedServer) {
+class ChampionshipControllerTest(private val server: EmbeddedServer) {
 
     @Test
-    fun testServerIsRunning() {
-        assert(embeddedServer.isRunning())
+    fun `Server is running`() {
+        assert(server.isRunning)
     }
 
     @Test
-    fun testIndex() {
-        val client: RxHttpClient = embeddedServer.applicationContext.createBean(RxHttpClient::class.java, embeddedServer.url)
-        assertEquals(HttpStatus.OK, client.toBlocking().exchange("/championships", String::class.java).status())
+    fun `Found championship by ID`() {
+        val client: RxHttpClient = server.applicationContext.createBean(RxHttpClient::class.java, server.url)
+        val response = client.toBlocking().exchange("/championships/924", String::class.java)
+        assertEquals(HttpStatus.OK, response.status())
+        println(response.body())
         client.close()
+    }
+
+    @Test
+    fun `Not found championship by ID`() {
+        val client: RxHttpClient = server.applicationContext.createBean(RxHttpClient::class.java, server.url)
+        val response = try {
+            client.toBlocking().exchange("/championships/925", String::class.java)
+        } catch (ex: HttpClientResponseException) {
+            ex.response
+        } finally {
+            client.close()
+        }
+        assertEquals(HttpStatus.NOT_FOUND, response.status())
+        println(response.body())
     }
 }
