ca5e08fab895eedddbc45cf4dda012be8423d742
Tiago Angelo <kurtis.angelo@gmail.com>
2020-08-04 19:20:31 -0300

    Team answer for each version
    Signed-off-by: Tiago Angelo <kurtis.angelo@gmail.com>

--- teams/routes/index.js
+++ teams/routes/index.js
@@ -4,6 +4,23 @@ const fs = require('fs');
 const teams = JSON.parse(fs.readFileSync(`${process.env.DATA_DIR}/teams.json`));
 const router = express.Router();
 
+class TeamV1 {
+    constructor({name, _links}) {
+        this.name = name;
+        this._links = _links
+    }
+}
+
+class TeamV2 {
+    constructor({name, image_url, _links}) {
+        this.name = name;
+        this.image_url = image_url
+        this._links = _links
+    }
+}
+
+const Team = process.env.VERSION === 'v2' ? TeamV2 : TeamV1
+
 const getTeams = async (req, res, next) => {
     try {
         res.json(teams);
@@ -14,7 +31,9 @@ const getTeams = async (req, res, next) => {
 
 const getTeam = async (req, res, next) => {
     try {
-        const team = teams._embedded.teams.filter(({_links}) => _links.team.href === `/teams/${req.params.id}`)
+        const team = teams._embedded.teams
+            .filter(({_links}) => _links.team.href === `/teams/${req.params.id}`)
+            .map(value => new Team(value))
         if (!team || team.length === 0) {
             const err = new Error('Team not found');
             err.status = 404;
