4b065871633588acba98addad2090778b2c05962
Tiago Angelo <kurtis.angelo@gmail.com>
2020-08-16 16:50:22 -0300

    Fixing image build

--- bets/docker-build.sh
+++ bets/docker-build.sh
@@ -6,4 +6,4 @@ docker build . -t kurtis/bets:v1
 echo
 echo
 echo "To run the docker container execute:"
-echo "    $ docker run -p 9090:9090 kurtis/bets:v1"
+echo "    $ docker run --rm -p 9090:9090 kurtis/bets:v1"
