0ce41ce94d19244a875790a5f08c3f07810858a4
Tiago Angelo <kurtis.angelo@gmail.com>
2020-11-08 11:41:33 -0300

    Building a native executable

--- matches/docker-build.sh
+++ matches/docker-build.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-./mvnw clean package -Dquarkus.container-image.build=true -DskipTests
+./mvnw package -Pnative -Dquarkus.native.container-build=true -DskipTests
+docker build -f src/main/docker/Dockerfile.native -t kurtis/matches:v1 .
+
 echo
 echo
 echo "To run the docker container execute:"
-echo "    $ docker run --rm -v \$(pwd):/usr/src/data -p 8080:8080 kurtis/matches:v1"
+echo "    $ docker run --rm -p 8080:8080 kurtis/matches:v1"
